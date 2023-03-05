7239e3df304f6b673f58d29f09e2aa8a2fc13701
Simon Ingeson <44818+smonn@users.noreply.github.com>
2023-03-04 23:02:28 -0500

    more fixes

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -7,7 +7,7 @@
  * You might need to authenticate with NPM before running this script.
  */
 
-import * as devkit from '@nrwl/devkit';
+import devkit from '@nrwl/devkit';
 import { execSync } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
 import chalk from 'chalk';
